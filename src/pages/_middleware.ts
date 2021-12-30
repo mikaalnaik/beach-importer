f39c5703cccec74551bf449e64550a3975941e36
Mikaal <matthewnaik@gmail.com>
2021-12-29 20:37:21 -0500

    auth i believe

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization')
+
+  if (basicAuth) {
+    const auth = basicAuth.split(' ')[1]
+
+
+    if (auth === process.env.API_SECRET_KEY) {
+      return NextResponse.next()
+    }
+  }
+
+  return new Response('Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
