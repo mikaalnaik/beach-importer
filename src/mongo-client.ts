2b876ae867de48085be291a2e8de1306d94061ea
Mikaal <matthewnaik@gmail.com>
2021-12-30 22:44:07 -0500

    minor refactor to see if we can hit underneath the 5s timeout

--- src/mongo-client.ts
+++ src/mongo-client.ts
@@ -2,15 +2,14 @@ import { MongoClient } from 'mongodb';
 
 let cachedDb: MongoClient;
 
-
-export async function connectToDatabase() {
+export function connectToDatabase() {
   if (cachedDb) {
     return cachedDb;
   }
   if (process.env.MONGO_URL) {
     const client = new MongoClient(process.env.MONGO_URL);
     cachedDb = client;
-    return await client.connect();
+    return client.connect();
   } else {
     throw new Error('MongoDB connection url not provided');
   }
